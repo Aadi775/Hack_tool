b24ebf0a597abfb0e50450fd7e354fb87a19e529
Aadi775 <hitarth112006@gmail.com>
2021-02-19 15:42:04 +0530

    first commit

---
+++ TCPclient.py
@@ -0,0 +1,24 @@
+import socket
+from main import get_ip
+
+HEADER = 64
+port = 5050
+server = get_ip()
+Addr = (server, port)
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(Addr)
+
+def send(msg):
+    message = msg.encode("utf-8")
+    msg_lenght= msg.length()
+    send_length= str(msg_lenght).encode("utf-8")
+    send_length+=b' '* (HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(999).decode())
+
+while True:
+    a = input()
+    send(a)
---
+++ main.py
@@ -0,0 +1,39 @@
+import socket
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+print(get_ip())
+
+def takeshot():
+    import win32gui
+    import win32ui
+    import win32con
+    import win32api
+
+    hdesktop = win32gui.GetDesktopWindow()
+
+    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
+    desktop_dc = win32gui.GetWindowDC(hdesktop)
+    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+
+    screenshot = win32ui.CreateBitmap()
+    screenshot.CreateCompatibleBitmap(img_dc, width, height)
+    mem_dc.SelectObject(screenshot)
+    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+    screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\logo.bmp')
+
+    mem_dc.DeleteDC()
+    win32gui.DeleteObject(screenshot.GetHandle())
