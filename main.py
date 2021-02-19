fcc0f50c80295ea13e1950861d3dd7c6870d3596
Aadi775 <hitarth112006@gmail.com>
2021-02-19 16:04:07 +0530

    commit

--- main.py
+++ main.py
@@ -37,3 +37,63 @@ def takeshot():
 
     mem_dc.DeleteDC()
     win32gui.DeleteObject(screenshot.GetHandle())
+
+
+
+
+
+
+
+
+
+
+
+
+
+#while True:
+
+#     read_sockets, write_sockets, error_sockets = select.select(connected_clients_sockets, [], [])
+#
+#     for sock in read_sockets:
+#
+#         if sock == server_socket:
+#
+#             sockfd, client_address = server_socket.accept()
+#             connected_clients_sockets.append(sockfd)
+#
+#         else:
+#             try:
+#
+#                 data = sock.recv(4096)
+#                 txt = str(data)
+#
+#                 if txt.startswith('SIZE'):
+#                     tmp = txt.split()
+#                     size = int(tmp[1])
+#
+#                     print 'got size'
+#
+#                     sock.send("GOT SIZE")
+#
+#                 elif txt.startswith('BYE'):
+#                     sock.shutdown()
+#
+#                 elif data:
+#
+#                     myfile = open(basename % imgcounter, 'wb')
+#
+#                     data = sock.recv(40960000)
+#                     if not data:
+#                         myfile.close()
+#                         break
+#                     myfile.write(data)
+#                     myfile.close()
+#
+#                     sock.send("GOT IMAGE")
+#                     sock.shutdown()
+#             except:
+#                 sock.close()
+#                 connected_clients_sockets.remove(sock)
+#                 continue
+#         imgcounter += 1
+# server_socket.close()
